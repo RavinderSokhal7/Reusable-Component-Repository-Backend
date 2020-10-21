57e86c629fc6cb664c67291211712b630fd26f22
Ravinder Sokhal <sokhal.ravinder7@gmail.com>
2020-10-21 19:10:27 +0530

    Reusable-Component-Repository, Starts

---
+++ rcl-backend/src/main/java/com/rcl/rclbackend/RclBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rcl.rclbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RclBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RclBackendApplication.class, args);
+	}
+
+}
---
+++ rcl-backend/src/test/java/com/rcl/rclbackend/RclBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcl.rclbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RclBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
